cdc2c08bde7484052fe9ac130a584fad8d0d38a4
uditanshutomar <uditanshutomar21@gmail.com>
2026-01-30 06:04:42 -0700

    custosa 1.0.6

--- Casks/custosa.rb
+++ Casks/custosa.rb
@@ -1,6 +1,6 @@
 cask "custosa" do
-  version "1.0.4"
-  sha256 "77db276e97ead302a52f04125966bdd075afe4848c0a0d18cff2ae9407b03b85"
+  version "1.0.6"
+  sha256 "117c6544a10cfabd029c0698faa7289f18df9ffdb6db901e2d1c2af40b1208b9"
 
   url "https://github.com/uditanshutomar/CustosaXopenclaw/releases/download/#{version}/CustosaXopenclaw.zip"
   name "Custosa"
@@ -10,10 +10,16 @@ cask "custosa" do
   depends_on macos: ">= :ventura"
 
   app "Custosa.app"
+  binary "#{appdir}/Custosa.app/Contents/MacOS/Custosa", target: "custosa"
 
   postflight do
     system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{appdir}/Custosa.app"]
-    system_command "/bin/sh", args: ["-c", "/usr/bin/open -a Custosa >/dev/null 2>&1 &"], sudo: false
+    system_command "/usr/bin/open", args: ["-a", "Custosa"], must_succeed: false
+    console_uid = File.stat("/dev/console").uid
+    system_command "/bin/launchctl",
+                   args: ["asuser", console_uid.to_s, "/usr/bin/open", "-a", "Custosa"],
+                   sudo: true,
+                   must_succeed: false
   end
 
   uninstall launchctl: "com.custosa.proxy"
