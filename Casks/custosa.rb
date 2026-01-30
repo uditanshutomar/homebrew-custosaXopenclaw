f7867aadb26236dcd7d4051faec4c5c10b2efa8d
uditanshutomar <uditanshutomar21@gmail.com>
2026-01-30 02:55:01 -0700

    Update cask SHA for latest ZIP

--- Casks/custosa.rb
+++ Casks/custosa.rb
@@ -1,6 +1,6 @@
 cask "custosa" do
   version "1.0.0"
-  sha256 "954a2fa70a3009a6cad323cad6b6765693bfcd79d1706faf1ceedf2739fa31d8"
+  sha256 "3e52f447fad04c8219e25f022440e9fec3219a811db5806965a834eacc9b91be"
 
   url "https://github.com/uditanshutomar/CustosaXopenclaw/releases/download/#{version}/CustosaXopenclaw.zip"
   name "Custosa"
