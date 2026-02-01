8c61197fe3ead148ed99a4426a997df4d0d3b114
uditanshutomar <uditanshutomar21@gmail.com>
2026-02-01 06:01:40 -0700

    Update SHA256 for 1.1.0

--- Formula/custosa.rb
+++ Formula/custosa.rb
@@ -4,7 +4,7 @@ class Custosa < Formula
   desc "Prompt Injection Protection for OpenClaw/Moltbot"
   homepage "https://github.com/uditanshutomar/CustosaXopenclaw"
   url "https://github.com/uditanshutomar/CustosaXopenclaw/archive/refs/tags/1.1.0.tar.gz"
-  sha256 "93385050ca6a2c6e3b2a0c50f42771584aa36867c25f54cfddc06d3ab0036881"
+  sha256 "10603c641fcbef0375c432e9360126a5ce01ba16dc669c433b72eaeb9e120879"
   license :cannot_represent
 
   depends_on "python@3.12"
